7f40ddc467de9841f161554bc196dfd0d12c0ac6
Kairee G <89224268+Kaireega@users.noreply.github.com>
2024-09-18 15:39:30 -0400

    test
    test

---
+++ defs.py
@@ -0,0 +1,8 @@
+API_KEY = "f092735885a6ea06b0941160eb4855bb-ecdf07214bee956d3eb9d5dd1a618a7e"
+ACCOUNT_ID = "101-001-23541205-001"
+OANDA_URL = 'https://api-fxpractice.oanda.com/v3'
+
+SECURE_HEADER = {
+    'Authorization': f'Bearer {API_KEY}'
+}
+
