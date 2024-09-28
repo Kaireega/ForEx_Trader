790720d1cb43e5e6a7d0dd8d86d60af606ad909f
Kairee G <89224268+Kaireega@users.noreply.github.com>
2024-09-27 22:12:11 -0400

    added candle plot functions

--- insturment.py
+++ insturment.py
@@ -3,10 +3,10 @@ import utils
 
 class Instrument():
     def __init__(self, ob):
-        self.name = ob['name'],
-        self.type = ob['type'],
-        self.displayName = ob['displayName'],
-        self.pipLocation = pow(10, ob['pipLocation']),
+        self.name = ob['name']
+        self.type = ob['type']
+        self.displayName = ob['displayName']
+        self.pipLocation = pow(10, ob['pipLocation'])
         self.marginRate = ob['marginRate']
 
     def __repr__(self) -> str:
@@ -21,7 +21,26 @@ class Instrument():
     def get_instruments_list(cls):
         df = cls.get_intsrument_df()
         return [Instrument(x)for x in df.to_dict(orient= 'records')]
+    
+    @classmethod
+    def get_intsruments_dict(cls):
+        i_list =cls.get_instruments_list()
+        i_keys = [x.name for x in i_list]
+        return {k:v for (k,v)in zip(i_keys,i_list)}
+
+
+    @classmethod
+    def get_instrument_by_name(cls, pairname):
+        d = cls.get_intsruments_dict()
+        if pairname in d:
+            return d[pairname]
+        else: 
+            return None
 
 
 if __name__ == "__main__":
-    print(Instrument.get_instruments_list())
+#    for k,v in Instrument.get_intsruments_dict().items():
+#         print(k,v)
+    
+   
+   print(Instrument.get_instrument_by_name("EUR_USD"))
